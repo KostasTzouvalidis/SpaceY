33cf3b3da89758632fb3794f44a89a5486993960
Kostas Tzouvalidis <kostastzouvalidis@gmail.com>
2017-02-23 17:41:29 +0200

    Added script for shortcuts.

---
+++ SpaceY/Assets/Editor/UsefulShortcuts.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class UsefulShortcuts {
+	[MenuItem("Tools/Clear Console &#c")] //Alt + Shift + C
+	static void ClearConsole() {
+		var logEntries = System.Type.GetType ("UnityEditorInternal.LogEntries, UnityEditor.dll");
+		var clearMethod = logEntries.GetMethod ("Clear", System.Reflection.BindingFlags.Static |
+		                  System.Reflection.BindingFlags.Public);
+		clearMethod.Invoke (null, null);
+	}
+}
---
+++ SpaceY/Assets/Player_Master.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Master : MonoBehaviour {
+	
+	void OnEnable() {
+		
+	}
+	
+	void OnDisable() {
+		
+	}
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+	
+	private void InitializeReferences() {
+		
+	}
+}
