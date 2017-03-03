6ceffccd203bc03a2c0ed8ccb53cad1a17503acc
Kostas Tzouvalidis <kostastzouvalidis@gmail.com>
2017-03-04 00:55:29 +0200

    Configuring Spacecraft - Implemented Player Movement mechanics.\n Modified blender models' rotation to properly work in Unity.

---
+++ SpaceY/Assets/Scripts/Player_Input.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Input : MonoBehaviour {
+	private Player_Master playerMaster;
+	//TODO - GameManager_Master reference.
+
+	void Start () {
+		InitializeReferences ();		
+	}
+	
+	void Update () {
+		CheckIfCanMove ();
+	}
+
+	private void CheckIfCanMove() {
+		if(Input.GetMouseButtonDown(0))
+			playerMaster.CallEventInput ();
+	}
+	
+	private void InitializeReferences() {
+		playerMaster = GetComponent<Player_Master> ();
+	}
+}
---
+++ SpaceY/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+	private Player_Master playerMaster;
+	private Rigidbody myRig;
+	private float movementVelocitySqr;
+	[SerializeField]
+	private float movementVelocity = 10;
+	[SerializeField]
+	private float tiltByMovement = 2;
+
+	void OnEnable() {
+		InitializeReferences ();
+		playerMaster.EventInput += PlayerMove;
+	}
+	
+	void OnDisable() {
+		playerMaster.EventInput -= PlayerMove;
+	}
+
+	private void FixedUpdate() {
+		if (myRig.velocity.x != 0)
+			myRig.rotation = Quaternion.Euler(0.0f, 0.0f, myRig.velocity.x * -tiltByMovement);
+	}
+
+	private void PlayerMove() {
+		if (myRig.velocity.x < 0)
+			myRig.velocity = Vector3.right * movementVelocity;
+		else if (myRig.velocity.x > 0)
+			myRig.velocity = Vector3.left * movementVelocity;
+		else
+			myRig.velocity = Vector3.right * movementVelocity;
+	}
+
+	private void InitializeReferences() {
+		playerMaster = GetComponent<Player_Master> ();
+		myRig = GetComponent<Rigidbody> ();
+		movementVelocitySqr = movementVelocity * movementVelocitySqr;
+	}
+}
