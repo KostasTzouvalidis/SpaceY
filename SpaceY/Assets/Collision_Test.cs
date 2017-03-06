8395b5b58d160a24d9e9a1b3cd23f296c6eec180
Kostas Tzouvalidis <kostastzouvalidis@gmail.com>
2017-03-06 18:37:11 +0200

    Added VFX Particle Effects for simulation of velocity.

---
+++ SpaceY/Assets/Collision_Test.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision_Test : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider col) {
+		if (col.name == "Spacecraft")
+			Debug.Log("asd");
+	}
+}
