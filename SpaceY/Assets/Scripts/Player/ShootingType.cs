4f7efee76a37b48a4c15937f2888db3739ec97e4
Kostas Tzouvalidis <kostastzouvalidis@gmail.com>
2017-05-06 18:58:23 +0300

    Start implement shooting upgrade system.

---
+++ SpaceY/Assets/Scripts/Player/ShootingType.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ShootingType : MonoBehaviour {
+
+	private int numberOfProjectiles;
+	private GameObject projectile;
+
+	protected abstract void UpgradeShooting ();
+	protected abstract void DowngradeShooting ();
+	protected abstract void Shoot ();
+}
